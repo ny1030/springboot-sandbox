b0765598d2632e541853bd6684db083056e0c79c
ny1030 <ny1030biz@gmail.com>
2020-01-03 23:16:41 +0900

    create main class

---
+++ src/main/java/org/example/CrudApp.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CrudApp.class, args);
+    }
+}
